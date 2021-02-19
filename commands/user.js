304cd1fa75b8cf20784a2820de978d1e39878f65
Peter Ho <pho@buhao.cc>
2021-02-18 21:44:29 -0500

    add user args to %user

--- commands/user.js
+++ commands/user.js
@@ -4,16 +4,32 @@ module.exports = {
     name: 'user',
     description: 'User Info',
     execute(client, msg, args) {
-        let member = msg.guild.members.cache.get(msg.author.id);
+        if (!msg.mentions.users.size) {
+            let member = msg.guild.members.cache.get(msg.author.id);
 
-        let ret_embed = util.make_embed("User Info", msg.author);
-        ret_embed.addField("Your username", msg.author.tag);
-        ret_embed.addField("Your ID", msg.author.id);
-        ret_embed.addFields(
-            { name: "Joined", value: member.joinedAt.toLocaleString(), inline: true },
-            { name: "Registered", value: msg.author.createdAt.toLocaleString(), inline: true }
-        );
-        ret_embed.addField("Your avatar", `<${msg.author.displayAvatarURL({format:"png", dynamic:true})}>`);
-        msg.channel.send(ret_embed);
+            let ret_embed = util.make_embed("User Info", msg.author);
+            ret_embed.addField("Your username", msg.author.tag);
+            ret_embed.addField("Your ID", msg.author.id);
+            ret_embed.addFields(
+                { name: "Joined", value: member.joinedAt.toLocaleString(), inline: true },
+                { name: "Registered", value: msg.author.createdAt.toLocaleString(), inline: true }
+            );
+            ret_embed.addField("Your avatar", `<${msg.author.displayAvatarURL({format:"png", dynamic:true})}>`);
+            msg.channel.send(ret_embed);
+        } else {
+            msg.mentions.users.map(user => {
+                let member = msg.guild.members.cache.get(user.id);
+
+                let ret_embed = util.make_embed("User Info", user);
+                ret_embed.addField("Your username", user.tag);
+                ret_embed.addField("Your ID", user.id);
+                ret_embed.addFields(
+                    { name: "Joined", value: member.joinedAt.toLocaleString(), inline: true },
+                    { name: "Registered", value: user.createdAt.toLocaleString(), inline: true }
+                );
+                ret_embed.addField("Your avatar", `<${user.displayAvatarURL({format:"png", dynamic:true})}>`);
+                msg.channel.send(ret_embed);
+            });
+        }
     },
 };
