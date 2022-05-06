be828a713b5096b0ac3cdb676d023b0be193d499
Peter Ho <pho@buhao.cc>
2022-05-06 19:38:47 -0400

    add playlist support

--- commands/play.js
+++ commands/play.js
@@ -1,27 +1,12 @@
 const config = require('../config.json');
 const ytdl = require('ytdl-core');
+//const {getInfo} = require('ytdl-getinfo');
+const ytpl = require('ytpl');
 
-async function play(conn, msg, args) {
-    let file;
-    if (args[0] == "ram") {
-        console.log("playing ram");
-        file = "ram.m4a";
-    } else {
-        video_info = await ytdl.getInfo(args[0]);
-        if (video_info.videoDetails.isLiveContent) {
-            // Live video does not always have
-            // "highest" stream audio quality
-            file = await ytdl(args[0]);
-        } else {
-            file = await ytdl(args[0], {
-                filter: 'audioonly',
-                highWaterMark: 1 << 25
-            });
-        }
-        console.log(args[0])
-    }
+var playlist_queue = require('../queue.js').play_queue;
 
-    const dispatcher = conn.play(file);
+function play_queue(conn, msg, queue) {
+    const dispatcher = conn.play(queue.shift());
     dispatcher.setVolumeLogarithmic(config["vol"] / 200.0);
     dispatcher.on('error', console.error);
     dispatcher.on('start', () => {
@@ -30,7 +15,72 @@ async function play(conn, msg, args) {
     dispatcher.on('debug', console.debug);
     dispatcher.on('finish', () => {
         //msg.react('🛑');
+        if (queue.length != 0) {
+            play_queue(conn, msg, queue);
+        }
     });
+}
+
+async function push_dl_url(url, queue)
+{
+    if (ytdl.validateURL(url)) {
+        video_info = await ytdl.getInfo(url);
+
+        let file;
+        if (video_info.videoDetails.isLiveContent) {
+            // Live video does not always have
+            // "highest" stream audio quality
+            file = await ytdl(url);
+        } else {
+            file = await ytdl(url, {
+                filter: 'audioonly',
+                highWaterMark: 1 << 25
+            });
+        }
+        queue.push(file);
+    }
+}
+
+async function play(conn, msg, args) {
+    let file;
+    if (args[0] == "ram") {
+        console.log("playing ram");
+        file = "ram.m4a";
+    } else {
+
+        try {
+            url = args[0];
+
+            // Three types: playlist, video, playlist+video
+            // Playlist+video should be treated as video (for now)
+
+            if (ytdl.validateURL(url)) {
+                // Do nothing, process as a regular video
+                await push_dl_url(url, playlist_queue);
+                console.log(playlist_queue.length);
+            } else if (ytpl.validateID(url)) {
+                playlist = await ytpl(url);
+
+                // Queue all videos
+
+                // Hack to at least queue 1
+                let queued = 0;
+                for (const item of playlist.items) {
+                    //console.log(item.shortUrl);
+                    if (queued === 0)
+                        await push_dl_url(item.shortUrl, playlist_queue);
+                    else
+                        push_dl_url(item.shortUrl, playlist_queue);
+                    ++queued;
+                };
+            }
+            console.log(args[0]);
+            play_queue(conn, msg, playlist_queue);
+        } catch (error) {
+            msg.reply(error.stack);
+        }
+    }
+
 }
 module.exports = {
     name: 'play',
---
+++ queue.js
@@ -0,0 +1,3 @@
+var play_queue = []
+
+module.exports.play_queue = play_queue;
